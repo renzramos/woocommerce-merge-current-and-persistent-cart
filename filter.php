ba01de07242dc0df7be184ee90130b20911f26df
Renz Ramos <im.renzramos@gmail.com>
2017-08-29 11:35:41 +0800

    Update filter.php

--- filter.php
+++ filter.php
@@ -1,5 +1,33 @@
+<?php
 function wc_custom_user_redirect( $redirect, $user ) {
-
+// 	// Get the first of all the roles assigned to the user
+// 	$role = $user->roles[0];
+// 	$dashboard = admin_url();
+// 	$myaccount = get_permalink( wc_get_page_id( 'myaccount' ) );
+// 	if( $role == 'administrator' ) {
+// 		//Redirect administrators to the dashboard
+// 		$redirect = $myaccount;
+// 	} elseif ( $role == 'shop-manager' ) {
+// 		//Redirect shop managers to the dashboard
+// 		$redirect = $myaccount;
+// 	} elseif ( $role == 'editor' ) {
+// 		//Redirect $myaccount to the dashboard
+// 		$redirect = $dashboard;
+// 	} elseif ( $role == 'author' ) {
+// 		//Redirect authors to the dashboard
+// 		$redirect = $myaccount;
+// 	} elseif ( $role == 'customer' || $role == 'subscriber' ) {
+// 		//Redirect customers and subscribers to the "My Account" page
+// 		$redirect = $myaccount;
+// 	} else {
+// 		//Redirect any other role to the previous visited page or, if not available, to the home
+// 		$redirect = wp_get_referer() ? wp_get_referer() : home_url();
+// 	}
+	
+// 	$redirect = wp_get_referer() ? wp_get_referer() : home_url();
+// 	return $redirect;
+    
+    
     if($_COOKIE['_redirect_url'] == '') {
        
         if (sizeof(WC()->cart->get_cart()) != 0) {
@@ -10,6 +38,8 @@ function wc_custom_user_redirect( $redirect, $user ) {
     }else{
         $redirect = $_COOKIE['_redirect_url'];
     }
+    
+    
     
     // declaration 
     $final_items_key = array();
@@ -86,3 +116,5 @@ function wc_custom_user_redirect( $redirect, $user ) {
     return $redirect;
 }
 add_filter( 'woocommerce_login_redirect', 'wc_custom_user_redirect', 10, 2 );
+
+?>
